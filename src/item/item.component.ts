422cd3b7cf365e2119daa1befc1e520616a45a29
Andysimmonsisme <andysimmons78@gmail.com>
2022-06-10 15:06:31 -0400

    added all item UI stuffs

--- src/item/item.component.ts
+++ src/item/item.component.ts
@@ -1,14 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-item',
+  selector: 'item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css'],
 })
 export class ItemComponent implements OnInit {
   constructor() {}
 
-  price: number;
+  @Input() id: number;
+  @Input() name: string;
+  @Input() price: number;
+  @Output() quantity: number = 0;
+
+  increaseQuantity() {
+    this.quantity++;
+  }
+
+  decreaseQuantity() {
+    if (this.quantity > 0) this.quantity--;
+  }
+
+  isNumber(evt) {
+    evt.preventDefault();
+    evt = evt ? evt : window.event;
+    var charCode = evt.which ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    this.quantity = charCode - 48; //char codes 48-56 represent numbers 0-9
+  }
 
   ngOnInit() {}
 }
