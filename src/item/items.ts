bdcdb7dff68948b8819127b9d27515dceeb00fc0
Andysimmonsisme <andysimmons78@gmail.com>
2022-06-11 10:19:35 -0400

    added all food images to item list, phase one is done!

--- src/item/items.ts
+++ src/item/items.ts
@@ -20,55 +20,64 @@ export const items = {
       id: 2,
       name: 'Bananas',
       price: 1.5,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Bananas.png?raw=true',
     },
     {
       id: 3,
       name: 'Orange',
       price: 0.5,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Oranges.png?raw=true',
     },
     {
       id: 4,
       name: 'Strawberries',
       price: 1,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Strawberries.png?raw=true',
     },
     {
       id: 5,
       name: 'Blueberries',
       price: 0.75,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Blueberries.png?raw=true',
     },
     {
       id: 6,
       name: 'Tomato',
       price: 0.35,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Tomatoes.png?raw=true',
     },
     {
       id: 7,
       name: 'Lettuce',
       price: 0.25,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Lettuce.png?raw=true',
     },
     {
       id: 8,
       name: 'Carrots',
       price: 0.1,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Carrots.png?raw=true',
     },
     {
       id: 9,
       name: 'Broccoli',
       price: 0.25,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Broccoli.png?raw=true',
     },
     {
       id: 10,
       name: 'Potato',
       price: 0.5,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Potatoes.png?raw=true',
     },
   ],
   //Meats--------------------
@@ -78,31 +87,36 @@ export const items = {
       id: 11,
       name: 'Steak',
       price: 5,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Steak.png?raw=true',
     },
     {
       id: 12,
       name: 'Ground Beef',
       price: 8,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Ground%20Beef.png?raw=true',
     },
     {
       id: 13,
       name: 'Pork Tenderloin',
       price: 10,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Pork%20Tenderloin.png?raw=true',
     },
     {
       id: 14,
       name: 'Chicken Breast',
       price: 6,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Chicken%20Breast.png?raw=true',
     },
     {
       id: 15,
       name: 'Salmon',
       price: 6,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Salmon.png?raw=true',
     },
   ],
   //Dairy--------------------
@@ -112,31 +126,36 @@ export const items = {
       id: 16,
       name: 'Milk',
       price: 1.5,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Milk.png?raw=true',
     },
     {
       id: 17,
       name: 'Cheese',
       price: 2,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Cheese.png?raw=true',
     },
     {
       id: 18,
       name: 'Yogurt',
       price: 2.5,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Yogurt.png?raw=true',
     },
     {
       id: 19,
       name: 'Butter',
       price: 1.5,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Butter.png?raw=true',
     },
     {
       id: 20,
       name: 'Sour Cream',
       price: 2,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Sour%20Cream.png?raw=true',
     },
   ],
   //Grains--------------------
@@ -146,31 +165,36 @@ export const items = {
       id: 21,
       name: 'Bread',
       price: 2.5,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Bread.png?raw=true',
     },
     {
       id: 22,
       name: 'Rice',
       price: 1,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Rice.png?raw=true',
     },
     {
       id: 23,
       name: 'Pasta',
       price: 2,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Pasta.png?raw=true',
     },
     {
       id: 24,
       name: 'Oatmeal',
       price: 1.5,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Oatmeal.png?raw=true',
     },
     {
       id: 25,
       name: 'Quinoa',
       price: 1.5,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Quinoa.png?raw=true',
     },
   ],
   //Miscellaneous--------------------
@@ -180,31 +204,36 @@ export const items = {
       id: 26,
       name: 'Eggs',
       price: 4,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Eggs.png?raw=true',
     },
     {
       id: 27,
       name: 'Flour',
       price: 2,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Flour.png?raw=true',
     },
     {
       id: 28,
       name: 'Sugar',
       price: 2.5,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Sugar.png?raw=true',
     },
     {
       id: 29,
       name: 'Coffee',
       price: 3,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Coffee.png?raw=true',
     },
     {
       id: 30,
       name: 'Almonds',
       price: 5,
-      imgUrl: '',
+      imgUrl:
+        'https://github.com/Andysimmonsisme/YouMart-Shopping-App/blob/master/src/assets/img/Almonds.png?raw=true',
     },
   ],
 };
